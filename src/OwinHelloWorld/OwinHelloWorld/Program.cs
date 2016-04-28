f3153e4bdf27dd421172c1e0621ee65c7bab8d84
ivanmartinvalle <ivanmartinvalle@gmail.com>
2016-04-28 11:23:46 -0500

    Add Hello, World! resource at http://localhost:9000/api/helloworld .

--- src/OwinHelloWorld/OwinHelloWorld/Program.cs
+++ src/OwinHelloWorld/OwinHelloWorld/Program.cs
@@ -1,15 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Web.Http;
+using Topshelf;
 
 namespace OwinHelloWorld
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static int Main(string[] args)
         {
+            return (int)HostFactory.Run(x =>
+            {
+                x.Service<OwinService>(s =>
+                {
+                    s.ConstructUsing(() => new OwinService());
+                    s.WhenStarted(service => service.Start());
+                    s.WhenStopped(service => service.Stop());
+                });
+            });
+        }
+    }
+
+    public class OwinService
+    {
+        private IDisposable _webApp;
+
+        public void Start()
+        {
+            _webApp = WebApp.Start<StartOwin>("http://localhost:9000");
+        }
+
+        public void Stop()
+        {
+            _webApp.Dispose();
+        }
+    }
+
+    public class StartOwin
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+                );
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+
+    public class HelloWorldController : ApiController
+    {
+        public string Get()
+        {
+            return "Hello, World!";
         }
     }
 }
